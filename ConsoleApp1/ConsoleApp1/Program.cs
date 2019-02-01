d0d92fb5c5aac3784f3393ad196df1c244acc078
ed pepera <edpepera@gmail.com>
2019-02-01 04:18:23 -0500

    first few lines checking

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//DECLARING VARIABLES  
+            string firstNAME;
+            string middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightinches;
+            double totalHeightCM;
+
+
+        }
+    }
+}
